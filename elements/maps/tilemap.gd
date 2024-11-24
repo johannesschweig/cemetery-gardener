d343a7d313134c67258a6a88672e7d59cbbceb83
Johannes Schweig <j.schweig@posteo.de>
2024-11-24 14:12:59 +0100

    added custom cursors

--- elements/maps/tilemap.gd
+++ elements/maps/tilemap.gd
@@ -2,23 +2,19 @@ extends TileMapLayer
 
 var name_label
 var name_panel
+var cursor = load("res://assets/cursor.png")
+var pointer = load("res://assets/pointer.png")
 
 func _ready() -> void:
 	name_panel = %PoiLabelPanel
 	name_label = %PoiLabel
+	self.remove_child(name_panel)
 	#name_panel.set_anchors_preset(Control.PRESET_CENTER) # TODO does not work
-	print(get_cell_tile_data(Vector2i(14, 8)))
-	print(get_cell_tile_data(Vector2i(14, 9)))
-	print(get_cell_source_id(Vector2i(14,8)))
-	print(get_cell_source_id(Vector2i(14,9)))
-	print(get_tile_map_data_as_array())
-	
 
 func _process(_delta):
 	var mouse_pos_global = get_viewport().get_mouse_position()
 	var tile_pos = local_to_map(mouse_pos_global)
 	var tile_data = get_cell_tile_data(tile_pos)
-	print(tile_pos)
 	if tile_data is TileData:
 		var name = GameManager.getTitleFromIdentifier(tile_data.get_custom_data("name"))
 		if !self.get_children() and name:
@@ -26,6 +22,9 @@ func _process(_delta):
 			# center the label
 			name_panel.position = map_to_local(tile_pos) - Vector2((len(name) * 17) / 2, 20)
 			add_child(name_panel)
+			# change cursor
+			Input.set_custom_mouse_cursor(pointer)
 	else:
+		Input.set_custom_mouse_cursor(cursor)
 		if self.get_children():
 			self.remove_child(name_panel)

1a2dbb42148d48f4bd326503aed0767cd1fb60e4
Johannes Schweig <j.schweig@posteo.de>
2024-12-04 11:53:01 +0100

    translation toggle buttons

--- elements/intro/intro.gd
+++ elements/intro/intro.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
-
 func _on_continue_pressed() -> void:
 	get_parent().next_stage()
 
-
 func _on_english_pressed() -> void:
-	TranslationServer.set_locale("en")
+	TranslationServer.set_locale('en')
 
 
 func _on_german_pressed() -> void:
-	TranslationServer.set_locale("de")
+	TranslationServer.set_locale('de')
